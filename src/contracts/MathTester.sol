d395394f046655d6d4305503eb1a65fa94d91ac9
Cory Huebner <chuebner86@gmail.com>
2024-06-28 14:52:42 -0500

    #6218 - doing that trader joe math yall

--- src/contracts/MathTester.sol
+++ src/contracts/MathTester.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {PriceHelper} from "./libraries/trader-joe/PriceHelper.sol";
+import {SafeCast} from "./libraries/trader-joe/SafeCast.sol";
+
 /**
- * Oftentimes, we come across some confusing math equation
+ * Oftentimes, we come across sn
  * in smart contracts. This contract just allows us to plug
  * in those math equations and test them
  */
 contract MathTester {
+  using SafeCast for uint256;
+
   /**
    * Test Solidity's left-shift operator; found pretty heavily in TraderJoe
    * @param num The number to be left-shifted
@@ -14,5 +19,109 @@ contract MathTester {
    */
   function shiftLeft(uint256 num, uint256 shift) public pure returns (uint256) {
     return num << shift;
+  }
+
+  struct TraderJoePrice {
+    uint256 rawPrice;
+    uint256 decimalPrice;
+  }
+
+  /**
+   * TraderJoe uses a 128.128-binary fixed-point number system for prices.
+   * They also use "bins" to hold liquidity at discrete prices. This
+   * function recreates their price calculation logic for a given bin and tries to
+   * give more human-readable output as for a price along with the
+   * TraderJoe derived price
+   * @param binId The id of the bin
+   * @param binStep The bin step
+   * @return traderJoePrice The calculated TraderJoe price
+   */
+  function getTraderJoeBinPrice(
+    uint24 binId,
+    uint16 binStep
+  ) public pure returns (TraderJoePrice memory traderJoePrice) {
+    uint256 _rawPrice = PriceHelper.getPriceFromId(binId, binStep);
+    uint256 _decimalPrice = PriceHelper.convert128x128PriceToDecimal(_rawPrice);
+    TraderJoePrice memory _traderJoePrice = TraderJoePrice(_rawPrice, _decimalPrice);
+    return _traderJoePrice;
+  }
+
+  struct DeltaPrimeTraderJoeBinDetails {
+    TraderJoePrice traderJoePrice;
+    uint256 liquidity;
+    uint256 valuation;
+  }
+
+  function getTokenXTotalBigPrice(
+    uint8 decimals,
+    uint256 traderJoeDecimalPrice,
+    uint256 oraclePrice,
+    uint256 liquidity,
+    uint256 debtCoverage
+  ) public pure returns (uint256) {
+    uint256 weightedLiquidity = debtCoverage * liquidity;
+    uint256 tokenDecimalMultiplier = 10 ** decimals;
+    uint256 traderJoeDecimaledPrice = traderJoeDecimalPrice / 10 ** 18;
+    return
+      ((weightedLiquidity / traderJoeDecimaledPrice / tokenDecimalMultiplier) * oraclePrice) /
+      10 ** 8;
+  }
+
+  function getTokenXTotalSmallPrice(
+    uint8 decimals,
+    uint256 traderJoeDecimalPrice,
+    uint256 oraclePrice,
+    uint256 liquidity,
+    uint256 debtCoverage
+  ) public pure returns (uint256) {
+    return
+      (((((debtCoverage * liquidity) / traderJoeDecimalPrice) * 10 ** 18) / 10 ** decimals) *
+        oraclePrice) / 10 ** 8;
+  }
+
+  function getTraderJoeValue(
+    uint8 decimals,
+    uint256 traderJoeDecimalPrice,
+    uint256 oraclePrice,
+    uint256 liquidity,
+    uint256 debtCoverage
+  ) public pure returns (uint256) {
+    return
+      traderJoeDecimalPrice > 10 ** 24
+        ? getTokenXTotalBigPrice(
+          decimals,
+          traderJoeDecimalPrice,
+          oraclePrice,
+          liquidity,
+          debtCoverage
+        )
+        : getTokenXTotalSmallPrice(
+          decimals,
+          traderJoeDecimalPrice,
+          oraclePrice,
+          liquidity,
+          debtCoverage
+        );
+  }
+
+  function getDeltaPrimeTraderJoeBinDetails(
+    uint24 binId,
+    uint16 binStep,
+    uint8 tokenXDecimals,
+    uint256 tokenXDebtCoverage,
+    uint256 tokenXOraclePrice,
+    uint256 binReserveX,
+    uint256 binReserveY
+  ) public pure returns (DeltaPrimeTraderJoeBinDetails memory) {
+    TraderJoePrice memory traderJoePrice = getTraderJoeBinPrice(binId, binStep);
+    uint256 liquidity = (traderJoePrice.decimalPrice * binReserveX) / 10 ** 18 + binReserveY;
+    uint256 valuation = getTraderJoeValue(
+      tokenXDecimals,
+      traderJoePrice.decimalPrice,
+      tokenXOraclePrice,
+      liquidity,
+      tokenXDebtCoverage
+    );
+    return DeltaPrimeTraderJoeBinDetails(traderJoePrice, liquidity, valuation);
   }
 }
--- src/hardhat-tests/math.test.ts
+++ src/hardhat-tests/math.test.ts
@@ -12,6 +12,39 @@ describe("Math tests", () => {
     return { contract, owner };
   }
 
+  /**
+   * A test case taken from an actual Trader Joe bin created via DeltaPrime.
+   * Trader Joe Bin Id: 8380293
+   * Trader Joe Bin Steps: 10
+   * LB Contract: https://arbiscan.io/address/0x0be4ac7da6cd4bad60d96fbc6d091e1098afa358#readProxyContract
+   * Transaction adding liquidity from smart loan: https://arbiscan.io/tx/0x7cc31f825c6869ab24e9132b42bdabea75d361c74b0eb3db48f9f24c30db1454
+   */
+  function getTraderJoeBinTestCase() {
+    // The id of the bin containing a subset of the invested assets at a given price point.
+    const binId = 8380293;
+    // The bin step (see https://docs.traderjoexyz.com/concepts/concentrated-liquidity#bin-pricing)
+    const binSteps = 10;
+    // The address of the first token in the liquidity pair
+    const tokenX = "0x912CE59144191C1204E64559FE8253a0e49E6548";
+    // The amount of token x in the bin
+    const binTokenXReserves = BigInt("1920399509864690844257");
+    // The address of the second token in the liquidity pair
+    const tokenY = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1";
+    // The amount of token y in the bin (zero in this case)
+    // See: https://docs.traderjoexyz.com/concepts/bin-liquidity#market-aggregation as to why reserves are often zero
+    const binTokenYReserves = BigInt("0");
+    return {
+      binId,
+      binSteps,
+      tokenX,
+      tokenXDecimals: 18,
+      binTokenXReserves,
+      tokenY,
+      tokenYDecimals: 18,
+      binTokenYReserves,
+    };
+  }
+
   describe("leftShifting", () => {
     [
       { shift: 0, expected: 1 },
@@ -43,6 +76,72 @@ describe("Math tests", () => {
 
         expect(result).to.equal(expected);
       });
+    });
+  });
+
+  describe("getTraderJoeBinPrice", () => {
+    it("should return derived TraderJoe price", async () => {
+      const { contract } = await loadFixture(deployMathTester);
+      const { binId, binSteps } = getTraderJoeBinTestCase();
+
+      const result = await contract.getTraderJoeBinPrice(binId, binSteps);
+
+      expect(result.rawPrice).to.equal(BigInt("83653709612360649898757686803092462"));
+      expect(result.decimalPrice).to.equal(BigInt("245836157686645"));
+    });
+  });
+
+  describe("getDeltaPrimeTraderJoeBinDetails", () => {
+    /**
+     * An extension of the base Trader Joe test case
+     * that also includes DeltaPrime specific data (e.g. debt coverage ratios)
+     */
+    function getDeltaPrimeTraderJoeBinTestCase() {
+      // Pulled from DeltaPrime's token manager
+      // debtCoverage=<borrowing power ratio>
+      // See: https://docs.deltaprime.io/prime-brokerage-account/health-and-borrowing-power/calculations
+      // Token manager: https://arbiscan.io/address/0x0a0d954d4b0f0b47a5990c0abd179a90ff74e255#code
+      const tokenXDebtCoverage = BigInt("833333333333333333");
+      const tokenYDebtCoverage = BigInt("833333333333333330");
+      const tokenXOraclePrice = BigInt("82817221");
+      const tokenYOraclePrice = BigInt("345473254471");
+      return {
+        ...getTraderJoeBinTestCase(),
+        tokenXDebtCoverage,
+        tokenXOraclePrice,
+        tokenYDebtCoverage,
+        tokenYOraclePrice,
+      };
+    }
+
+    it("should return derived TraderJoe price", async () => {
+      const { contract } = await loadFixture(deployMathTester);
+      const {
+        binId,
+        binSteps,
+        tokenXDecimals,
+        tokenXDebtCoverage,
+        tokenXOraclePrice,
+        binTokenXReserves,
+        binTokenYReserves,
+      } = getDeltaPrimeTraderJoeBinTestCase();
+
+      const result = await contract.getDeltaPrimeTraderJoeBinDetails(
+        binId,
+        binSteps,
+        tokenXDecimals,
+        tokenXDebtCoverage,
+        tokenXOraclePrice,
+        binTokenXReserves,
+        binTokenYReserves
+      );
+
+      expect(result.traderJoePrice.rawPrice).to.equal(
+        BigInt("83653709612360649898757686803092462")
+      );
+      expect(result.traderJoePrice.decimalPrice).to.equal(BigInt("245836157686645"));
+      expect(result.liquidity).to.equal(BigInt("472103636728451908"));
+      expect(result.valuation).to.equal(BigInt("1325351255139631512341"));
     });
   });
 });
