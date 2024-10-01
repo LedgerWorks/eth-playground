2b192e736a42a2725c5638789c746abd5eaa7082
Cory Huebner <chuebner86@gmail.com>
2024-10-01 14:59:50 -0500

    put back the gradient test of varied volatility/volume per liquidity numbers

--- src/hardhat-tests/adaptive-fees.test.ts
+++ src/hardhat-tests/adaptive-fees.test.ts
@@ -39,5 +39,39 @@ describe("AdaptiveFee", () => {
 
       expect(result).to.equal(12554);
     });
+
+    // Leaving this commented out in case we want to run it in the future
+    // it("should return expected fee results over a gradient of volatility/volume per liquidities", async () => {
+    //   const maxVolatility = BigInt("3395285563163");
+    //   const volatilitySteps = range(BigInt(0), maxVolatility, maxVolatility / BigInt(100));
+    //   const maxVolume = BigInt("829390885506055569978528");
+    //   const volumeSteps = range(BigInt(0), maxVolume, maxVolume / BigInt(100));
+    //   const { contract } = await loadFixture(deployAdaptiveFees);
+
+    //   const volatilityVolumeCombos = Array.from(arrayProduct(volatilitySteps, volumeSteps));
+
+    //   const feeConfiguration = {
+    //     alpha1: 4771,
+    //     alpha2: 7483,
+    //     beta1: 3219,
+    //     beta2: 12920,
+    //     gamma1: 2678,
+    //     gamma2: 3892,
+    //     volumeBeta: 5747,
+    //     volumeGamma: 3645,
+    //     baseFee: 300,
+    //   };
+    //   const results = await Promise.all(
+    //     volatilityVolumeCombos.map(async ([volatility, volumePerLiquidity]) => {
+    //       const fee = await contract.getFee(volatility, volumePerLiquidity, feeConfiguration);
+    //       return { volatility, volumePerLiquidity, fee };
+    //     })
+    //   );
+
+    //   await writeFile(
+    //     "/tmp/adaptive-fee-results.json",
+    //     JSON.stringify(results, ethJsonReplacer, 2)
+    //   );
+    // });
   });
 });
